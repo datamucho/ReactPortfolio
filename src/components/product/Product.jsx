7adda8c4d91ba98683c4ec4e732d274d3abc5a2e
Data Muchiashvili <datamucho3@gmail.com>
2022-01-26 21:07:38 +0400

    commit

---
+++ src/components/product/Product.jsx
@@ -0,0 +1,18 @@
+import "./product.css";
+
+const Product = ({img,link}) => {
+  return (
+    <div className="p">
+      <div className="p-browser">
+        <div className="p-circle"></div>
+        <div className="p-circle"></div>
+        <div className="p-circle"></div>
+      </div>
+      <a href={link}  >
+        <img src={img} alt="Web" className="p-img" />
+      </a>
+    </div>
+  );
+};
+
+export default Product;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/6927360/pexels-photo-6927360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    link: "https://datamucho.github.io/calculator/",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    link: "https://datamucho.github.io/Tindog/",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/3608804/pexels-photo-3608804.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    link: "https://datamucho.github.io/Drums/",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/1400136/pexels-photo-1400136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    link: "https://datamucho.github.io/Simon-Game/",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "https://datamucho.github.io/Portfolio/",
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/264547/pexels-photo-264547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    link: "https://datamucho.github.io/product_landing/",
+  },
+];
